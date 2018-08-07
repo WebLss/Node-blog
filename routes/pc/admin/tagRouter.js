1f3ebad796e83175d22e97cb1620e04081bcbce3
lishengsong <2353142548@qq.com>
2018-08-07 18:34:46 +0800

    .
    Signed-off-by: lishengsong <2353142548@qq.com>

--- routes/pc/admin/tagRouter.js
+++ routes/pc/admin/tagRouter.js
@@ -1,12 +1,20 @@
 const express = require('express')
+const path = require('path')
+const constant = require('../../../common/constant')
 const router = express.Router()
 const tagModel = require('../../../models/tagModel')
-/* GET users listing. */
-router.get('/', function (req, res, next) {
-  tagModel.createAndUpdate('xiaochuan', function (message) {
+/**
+ *  添加标签页
+ */
+router.get('/add', function (req, res, next) {
+  /* tagModel.createAndUpdate('xiaochuan', function (message) {
     console.log(message)
-  })
-  res.send('hah')
+  }) */
+  res.render(path.join(constant.view_dir, '/admin/tag/add'))
+})
+
+router.post('/add', function (req, res, next) {
+  console.log(req.body);
 })
 
 module.exports = router
